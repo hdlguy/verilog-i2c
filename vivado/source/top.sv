25f167a632b0ccd7850887bd223618fbd7f109c5
hdlguy <hdlguy@hdlguy.com>
2024-08-08 16:46:45 -0400

    tried to control the i2c_master_axil.v core with a Microblaze but failed to achieve robust control.

--- vivado/source/top.sv
+++ vivado/source/top.sv
@@ -6,8 +6,10 @@ module top (
     output  logic       usb_uart_txd,
     input   logic       usb_uart_rxd,
     //
-    output  logic       i2c_scl,
-    output  logic       i2c_sda
+    inout   logic       i2c_scl,
+    inout   logic       i2c_sda,
+    //
+    output  logic       led_blue, led_green, led_yellow, led_red
 );
 
     logic clk;
@@ -20,6 +22,10 @@ module top (
     end
     assign reset  =  reset_vec[0];
     assign resetn = ~reset_vec[0];
+    
+    logic[3:0] led;
+    assign led = 4'b1111;
+    assign {led_blue, led_green, led_yellow, led_red} = led;
 
     // i2c interface
     logic i2c_scl_i, i2c_scl_o, i2c_scl_t;
@@ -97,8 +103,8 @@ module top (
         .i2c_sda_t      (i2c_sda_t)
     );
     
-    OBUFT #(.DRIVE(12), .IOSTANDARD("DEFAULT"), .SLEW("SLOW")) OBUFT_i2c_scl (.O(i2c_scl), .I(i2c_scl_i), .T(i2c_scl_t));
-    OBUFT #(.DRIVE(12), .IOSTANDARD("DEFAULT"), .SLEW("SLOW")) OBUFT_i2c_sda (.O(i2c_sda), .I(i2c_sda_i), .T(i2c_sda_t));
+    IOBUF #(.DRIVE(12), .IBUF_LOW_PWR("TRUE"), .IOSTANDARD("DEFAULT"), .SLEW("SLOW")) IOBUF_i2c_scl (.O(i2c_scl_i), .IO(i2c_scl), .I(i2c_scl_o), .T(i2c_scl_t));
+    IOBUF #(.DRIVE(12), .IBUF_LOW_PWR("TRUE"), .IOSTANDARD("DEFAULT"), .SLEW("SLOW")) IOBUF_i2c_sda (.O(i2c_sda_i), .IO(i2c_sda), .I(i2c_sda_o), .T(i2c_sda_t));
     
 
     // Microblaze Processor Block Diagram
@@ -133,50 +139,7 @@ module top (
     );
     
     // debug
-    i2c_ila i2c_ila_inst (.clk(axi_aclk), .probe0({i2c_scl_i, i2c_scl_o, i2c_scl_t, i2c_sda_i, i2c_sda_o, i2c_sda_t}), .probe1({M00_awaddr[3:0], M00_wvalid, M00_araddr[3:0], M00_rvalid})); // 6, 10
+    i2c_ila i2c_ila_inst (.clk(axi_aclk), .probe0({i2c_scl_i, i2c_scl_t, i2c_sda_i, i2c_sda_t})); // 4
         
 endmodule
 
-/*
-module i2c_master_axil #
-(
-    parameter DEFAULT_PRESCALE = 1,
-    parameter FIXED_PRESCALE = 0,
-    parameter CMD_FIFO = 1,
-    parameter CMD_FIFO_DEPTH = 32,
-    parameter WRITE_FIFO = 1,
-    parameter WRITE_FIFO_DEPTH = 32,
-    parameter READ_FIFO = 1,
-    parameter READ_FIFO_DEPTH = 32
-) (
-    input  wire        clk,
-    input  wire        rst,
-    // axi lite interface
-    input  wire [3:0]  s_axil_awaddr,
-    input  wire [2:0]  s_axil_awprot,
-    input  wire        s_axil_awvalid,
-    output wire        s_axil_awready,
-    input  wire [31:0] s_axil_wdata,
-    input  wire [3:0]  s_axil_wstrb,
-    input  wire        s_axil_wvalid,
-    output wire        s_axil_wready,
-    output wire [1:0]  s_axil_bresp,
-    output wire        s_axil_bvalid,
-    input  wire        s_axil_bready,
-    input  wire [3:0]  s_axil_araddr,
-    input  wire [2:0]  s_axil_arprot,
-    input  wire        s_axil_arvalid,
-    output wire        s_axil_arready,
-    output wire [31:0] s_axil_rdata,
-    output wire [1:0]  s_axil_rresp,
-    output wire        s_axil_rvalid,
-    input  wire        s_axil_rready,
-    // i2c interface
-    input  wire        i2c_scl_i,
-    output wire        i2c_scl_o,
-    output wire        i2c_scl_t,
-    input  wire        i2c_sda_i,
-    output wire        i2c_sda_o,
-    output wire        i2c_sda_t
-);
-*/
--- vivado/vitis/src/i2c_test/test.c
+++ vivado/vitis/src/i2c_test/test.c
@@ -3,25 +3,70 @@
 #include "xil_printf.h"
 #include "fpga.h"
 
-int i2c_write(uint8_t addr, uint8_t* buf, int num) {
+int i2c_write(uint32_t* ptr, uint8_t addr, uint8_t* buf, int num) {
 	
-	return(0);
+	for (int i=0; i<(num-1); i++) {
+		ptr[I2C_MASTER_DATA] = buf[i];
+	}
+	ptr[I2C_MASTER_DATA] = buf[num-1] | I2C_DATA_LAST;	
+	
+    ptr[I2C_MASTER_COMMAND] = addr | I2C_COMMAND_WR_MULT;     
+    
+    while(ptr[I2C_MASTER_STATUS] & I2C_STATUS_BUSY);  // wait until i2c core not busy
+    
+//    ptr[I2C_MASTER_COMMAND] = addr | I2C_COMMAND_STOP;
+
+//    xil_printf("I2C_MASTER_STATUS =   0x%08x\n\r", ptr[I2C_MASTER_STATUS]);
+
+    if(ptr[I2C_MASTER_STATUS] & I2C_STATUS_MISS_ACK) {
+    	ptr[I2C_MASTER_STATUS] = 0xffffffff;  // clear status flags
+    	return(1);
+    } else {
+    	ptr[I2C_MASTER_STATUS] = 0xffffffff;  // clear status flags
+    	return(0);
+    }
 }
+
+
+int i2c_read(uint32_t* ptr, uint8_t addr, uint8_t* buf, int num) {
+
+    ptr[I2C_MASTER_COMMAND] = addr | I2C_COMMAND_READ;    
+	
+}
+
 
 int main()
 {
-
+	int status;
+	
     xil_printf("\n\rI2C Test\n\r");
     
-    uint32_t* i2c_base = (uint32_t *)XPAR_M00_BASEADDR;
-    uint8_t wr_buf[100], rd_buf[100];
+    uint32_t* i2c_base = (uint32_t *)XPAR_M00_BASEADDR; // verilog-i2c wired to axi bus M00
+    
+    uint8_t wr_buf[10], rd_buf[10];
 
-    i2c_base[I2C_MASTER_PRESCALE] = 250; // 100Khz operation
+    i2c_base[I2C_MASTER_PRESCALE] = 63; // 250 =100Khz operation, 63 = 400KHz
     
     xil_printf("I2C_MASTER_STATUS =   0x%08x\n\r", i2c_base[I2C_MASTER_STATUS]);
     xil_printf("I2C_MASTER_COMMAND =  0x%08x\n\r", i2c_base[I2C_MASTER_COMMAND]);
     xil_printf("I2C_MASTER_DATA =     0x%08x\n\r", i2c_base[I2C_MASTER_DATA]);
     xil_printf("I2C_MASTER_PRESCALE = 0x%08x\n\r", i2c_base[I2C_MASTER_PRESCALE]);
+    
+    
+//    wr_buf[0] = 0x30; wr_buf[1] = 0x41;    
+//    status = i2c_write(i2c_base, I2C_HUMIDITY, wr_buf, 2);
+//    xil_printf("status = 0x%08x\n\r", status);
+    
+    
+    wr_buf[0] = 0xF3; wr_buf[1] = 0x2D;  
+    status = i2c_write(i2c_base, I2C_HUMIDITY, wr_buf, 2);
+    xil_printf("status = 0x%08x\n\r", status);
+    
+
+    i2c_read(i2c_base, I2C_HUMIDITY, rd_buf, 2);
+
+    
+    xil_printf("test complete\n\r");
 
     return 0;
 }
